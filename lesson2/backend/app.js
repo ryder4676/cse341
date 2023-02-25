be5d3194252ccf942740259b4a29a26c0a00cba3
ryder4676 <60828482+ryder4676@users.noreply.github.com>
2023-02-25 10:44:39 -0700

    Initial commit for lesson2 branch

--- lesson2/backend/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express"); // Import the Express module
-const app = express(); // Create a new instance of the app
-const cors = require("cors"); // Import the CORS module
-
-app.use(cors({origin:"*", optionsSuccessStatus: 200})); // Set up CORS middleware to allow cross-origin requests from any origin
-app.use("/", require("./routes/index")); // Set up routing to use the index.js file in the routes folder for all requests to the root URL
-
-module.exports = app; // Export the app object so it can be used in other modules
