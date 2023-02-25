d825b6d37b939b6f26c9998073372a9d604b2d4a
ryder4676 <60828482+ryder4676@users.noreply.github.com>
2023-02-25 10:23:40 -0700

    lesson2

--- teamassignmentL2/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-
-app.use(cors({origin:'*', optionsSuccessStatus: 200}));
-app.use('/', require('./routes/index'));
-
-
-module.exports = app;
--- teamassignmentL2/index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get('/professional', require('../controller'));
-
-module.exports = router;
