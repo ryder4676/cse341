89abef96c6996599ba96a254f3dc76ef8b6620e8
ryder4676 <60828482+ryder4676@users.noreply.github.com>
2023-02-25 11:16:08 -0700

    moved folders

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-const awesomeFunction = (req, res, next) => {
-    res.json("Tammi Ronald");
-};
-const returnAnotherPerson = (req, res, next) => {
-    res.json("This is another awesome person!");
-};
-module.exports = { awesomeFunction, returnAnotherPerson };
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const routes = require("express").Router();
-const myController = require("../controllers")
-// this is by deafult home directory returns aewsome person
-routes.get("/", myController.awesomeFunction);
-// unlike this if you add /awesome to the GET request then it returns /awesome - returnAnotherPerson
-routes.get("/awesome", myController.returnAnotherPerson);
-
-module.exports = routes;
